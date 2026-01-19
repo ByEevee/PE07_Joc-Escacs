e5ca14b11c3bfbf8859976a7e6d27a2fecb30538
Ivan Carrasco <alu.ivan.carrasco@mataro.epiaedu.cat>
2026-01-19 20:48:44 +0100

    first commit

---
+++ EntregaParcial/escacs.java
@@ -0,0 +1,11 @@
+package EntregaParcial;
+
+public class escacs {
+    public static void main(String[] args) {
+        escacs game = new escacs();
+        game.main();
+    }
+    public void main() {
+        // Implementació del joc d'escacs
+    }
+}
