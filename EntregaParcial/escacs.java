d167424f5f4c88ef06193a8fa9f1526e9280a38c
Ivan Carrasco <alu.ivan.carrasco@mataro.epiaedu.cat>
2026-01-27 16:27:40 +0100

    Quart  commit, Inicialitzat tauler + impresió de tauler amb peces

--- EntregaParcial/escacs.java
+++ EntregaParcial/escacs.java
@@ -1,5 +1,6 @@
 package EntregaParcial;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -7,10 +8,28 @@ import java.util.Scanner;
 public class escacs {
     String nomJugador1;
     String nomJugador2;
+    String jugadorBlanques;
+    String jugadorNegres;
     int partidasjugades = 0;
+    String guanyadorAnterior = "";
+    
+    
+    char[][] tauler = new char[8][8];
+    String tornActual = "BLANQUES";
+    boolean partidaEnCurs = false;
+    
+    
+    ArrayList<Character> pecesMenjadesBlanques = new ArrayList<>();
+    ArrayList<Character> pecesMenjadesNegres = new ArrayList<>();
+    
+    
+    ArrayList<String> movimentsBlanques = new ArrayList<>();
+    ArrayList<String> movimentsNegres = new ArrayList<>();
+    
+    // Colors per consola
     public static final String RESET = "\u001B[0m";
     public static final String BOLD_WHITE = "\u001B[1;37m";
-    public static final String BOLD_BROWN = "\u001B[1;34m"; 
+    public static final String BOLD_BLUE = "\u001B[1;34m";
 
     public static void main(String[] args) {
         escacs game = new escacs();
@@ -34,9 +53,13 @@ public class escacs {
                         System.out.println("Els noms dels jugadors han estat registrats.");
                         System.out.println("Escollint els colors dels jugadors...");
                         escollirColor();
+                        
                         partidasjugades++;
                         
                     }
+                    inicialitzarTauler();
+                    imprimirTauler();
+                    partidasjugades++;
                     respostaCorrecta = true;
                     
                     break;
@@ -121,10 +144,66 @@ public class escacs {
 
         if (ordre == 1) {
             System.out.println("Jugador 1 comença amb " + BOLD_WHITE + "BLANQUES" + RESET);
-            System.out.println("Jugador 2 comença amb " + BOLD_BROWN + "NEGRES" + RESET);
+            System.out.println("Jugador 2 comença amb " + BOLD_BLUE + "NEGRES" + RESET);
         } else {
-            System.out.println("Jugador 1 comença amb " + BOLD_BROWN + "NEGRES" + RESET);
+            System.out.println("Jugador 1 comença amb " + BOLD_BLUE + "NEGRES" + RESET);
             System.out.println("Jugador 2 comença amb " + BOLD_WHITE + "BLANQUES" + RESET);
         }
     }
+    public void inicialitzarTauler() {
+        
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                tauler[i][j] = '.';
+            }
+        }
+        // Peces negres (files 0 i 1) - MINÚSCULES
+        tauler[0][0] = 't'; tauler[0][7] = 't'; // Torres
+        tauler[0][1] = 'c'; tauler[0][6] = 'c'; // Cavalls
+        tauler[0][2] = 'a'; tauler[0][5] = 'a'; // Alfils
+        tauler[0][3] = 'r'; // Reina (r minúscula)
+        tauler[0][4] = 'k'; // Rei
+        
+        for (int i = 0; i < 8; i++) {
+            tauler[1][i] = 'p'; // Peons negres
+        }
+        
+        // Peces blanques (files 6 i 7) - MAJÚSCULES
+        for (int i = 0; i < 8; i++) {
+            tauler[6][i] = 'P'; // Peons blancs
+        }
+        
+        tauler[7][0] = 'T'; tauler[7][7] = 'T'; // Torres
+        tauler[7][1] = 'C'; tauler[7][6] = 'C'; // Cavalls
+        tauler[7][2] = 'A'; tauler[7][5] = 'A'; // Alfils
+        tauler[7][3] = 'R'; // Reina (R majúscula)
+        tauler[7][4] = 'K'; // Rei
+        
+        // Reiniciar peces menjades i moviments
+        pecesMenjadesBlanques.clear();
+        pecesMenjadesNegres.clear();
+        movimentsBlanques.clear();
+        movimentsNegres.clear();
+        
+        tornActual = "BLANQUES";
+        partidaEnCurs = true;
+    }
+    public void imprimirTauler() {
+        System.out.println("\n  a b c d e f g h");
+        for (int i = 0; i < 8; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < 8; j++) {
+                char peca = tauler[i][j];
+                if (Character.isUpperCase(peca)) {
+                    System.out.print(BOLD_WHITE + peca + RESET + " ");
+                } else if (Character.isLowerCase(peca)) {
+                    System.out.print(BOLD_BLUE + peca + RESET + " ");
+                } else {
+                    System.out.print(peca + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
